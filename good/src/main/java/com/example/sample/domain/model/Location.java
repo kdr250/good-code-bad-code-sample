a571b5f2ff88e794b45b757718b3f9e9ab14a1b9
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-06-17 10:07:47 +0900

    位置を表現するクラスを作成

---
+++ good/src/main/java/com/example/sample/domain/model/Location.java
@@ -0,0 +1,29 @@
+package com.example.sample.domain.model;
+
+import lombok.Getter;
+
+/**
+ * 位置を表現するクラス
+ */
+@Getter
+public class Location {
+  private final int x;
+  private final int y;
+
+  public Location(final int x, final int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public Location shift(final int shiftX, final int shiftY) {
+    final int nextX = x + shiftX;
+    final int nextY = y + shiftY;
+    return new Location(nextX, nextY);
+  }
+
+  public Location shift(final Vector vector) {
+    final int nextX = x + vector.getX();
+    final int nextY = y + vector.getY();
+    return new Location(nextX, nextY);
+  }
+}
