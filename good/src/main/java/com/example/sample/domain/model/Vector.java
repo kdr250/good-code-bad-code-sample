2fb58eabe59adb9ce5fac8a37fb116d0a98bc9e7
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-06-18 21:20:04 +0900

    ベクトルに上下左右のファクトリメソッド追加

--- good/src/main/java/com/example/sample/domain/model/Vector.java
+++ good/src/main/java/com/example/sample/domain/model/Vector.java
@@ -10,8 +10,26 @@ public class Vector {
   private final int x;
   private final int y;
 
+  public static final Vector NONE = new Vector(0, 0);
+
   public Vector(final int x, final int y) {
     this.x = x;
     this.y = y;
+  }
+
+  public static Vector up(final int length) {
+    return new Vector(0, length * -1);
+  }
+
+  public static Vector down(final int length) {
+    return new Vector(0, length);
+  }
+
+  public static Vector left(final int length) {
+    return new Vector(length * -1, 0);
+  }
+
+  public static Vector right(final int length) {
+    return new Vector(length, 0);
   }
 }
