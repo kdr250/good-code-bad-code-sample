41b401c8add5ea6d7bcbee804dd63806d8f10de1
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-06-19 19:19:35 +0900

    sql修正

--- good/src/main/resources/schema.sql
+++ good/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tb_world_map (
     id INTEGER PRIMARY KEY,
-    vertical_size INTEGER NOT NULL,
     horizontal_size INTEGER NOT NULL,
+    vertical_size INTEGER NOT NULL,
     tiles TEXT NOT NULL
 );
