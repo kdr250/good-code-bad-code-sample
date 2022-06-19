d1f5ccac8644d271b12b7c903b4dc6fe538b36cf
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-06-19 18:19:54 +0900

    Tile作成

---
+++ good/src/main/java/com/example/sample/domain/model/Tile.java
@@ -0,0 +1,15 @@
+package com.example.sample.domain.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.awt.image.BufferedImage;
+
+@RequiredArgsConstructor
+@Getter
+public class Tile {
+  private final TileType tileType;
+  private final Location location;
+  private final BufferedImage bufferedImage;
+  // TODO: コリジョンも追加すること
+}
