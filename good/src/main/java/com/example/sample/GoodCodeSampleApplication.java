8d484ce831fb12c221f19780553324df1bac7761
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-06-22 09:36:15 +0900

    SpringBootApplicationとして実行するよう修正

--- good/src/main/java/com/example/sample/GoodCodeSampleApplication.java
+++ good/src/main/java/com/example/sample/GoodCodeSampleApplication.java
@@ -3,8 +3,33 @@
  */
 package com.example.sample;
 
+import com.example.sample.presentation.GamePanel;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+
+@SpringBootApplication
 public class GoodCodeSampleApplication {
   public static void main(String[] args) {
-    System.out.println("Good Code Sample!");
+    ConfigurableApplicationContext ctx = new SpringApplicationBuilder(GoodCodeSampleApplication.class)
+        .headless(false).run(args);
+
+    GamePanel gamePanel = ctx.getBean(GamePanel.class);
+
+    JFrame window = new JFrame();
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(false);
+    window.setTitle("2D Adventure");
+
+    window.add(gamePanel);
+
+    window.pack();
+
+    window.setLocationRelativeTo(null);
+    window.setVisible(true);
+
+    gamePanel.startGameThread();
   }
 }
