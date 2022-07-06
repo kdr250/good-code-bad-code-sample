5822d989f738a859a7e6bf49cdfdcc256f5f8bc8
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-07-07 00:28:00 +0900

    使用していないクラス削除、リネーム

--- good/src/main/java/com/example/sample/domain/model/event/EquipEvent.java
+++ good/src/main/java/com/example/sample/domain/model/event/EquipEvent.java
@@ -3,17 +3,17 @@ package com.example.sample.domain.model.event;
 import com.example.sample.domain.model.character.player.Player;
 import com.example.sample.domain.model.item.Equipment;
 
-public class EquipWeaponPlayerEvent implements PlayerEvent {
+public class EquipEvent implements PlayerEvent {
 
   private final Equipment equipment;
 
-  public EquipWeaponPlayerEvent(final Equipment equipment) {
+  public EquipEvent(final Equipment equipment) {
     this.equipment = equipment;
   }
 
   @Override
   public boolean execute(Player player) {
-    // TODO: Not yet implemented
+    player.changeEquipment(equipment);
     return true;
   }
 }
