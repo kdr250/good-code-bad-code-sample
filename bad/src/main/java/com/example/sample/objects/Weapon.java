000d273c207780cc69b03a98a563be2a19f4a382
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-07-20 18:30:15 +0900

    攻撃力を表示するよう修正

--- bad/src/main/java/com/example/sample/objects/Weapon.java
+++ bad/src/main/java/com/example/sample/objects/Weapon.java
@@ -3,6 +3,9 @@ package com.example.sample.objects;
 import com.example.sample.GamePanel;
 import com.example.sample.entity.Entity;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 /**
  * リスト4.7 武器を表現するクラス
  */
@@ -22,5 +25,25 @@ public class Weapon extends Entity {
   @Override
   public AttackPower attackPower() {
     return attackPower;
+  }
+
+  @Override
+  public void draw(Graphics2D g2) {
+    int screenX = location.worldX - panel.player.location.worldX + panel.player.screenX;
+    int screenY = location.worldY - panel.player.location.worldY + panel.player.screenY;
+    BufferedImage image = overwriteNumber(down1, attackPower.value);
+    g2.drawImage(image, screenX, screenY, null);
+  }
+
+  private static final Font arial20 = new Font("Arial", Font.PLAIN, 20);
+
+  private BufferedImage overwriteNumber(BufferedImage original, int number) {
+    BufferedImage image = new BufferedImage(original.getWidth(), original.getHeight(), original.getType());
+    image.setData(original.getData());
+    Graphics2D g2 = image.createGraphics();
+    g2.setColor(Color.white);
+    g2.setFont(arial20);
+    g2.drawString(String.valueOf(number), panel.tileSize - 15, panel.tileSize - 10);
+    return image;
   }
 }
